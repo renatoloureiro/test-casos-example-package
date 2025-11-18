a01aca302acc3eb01bc8e6a67308f9f6cd05a71e
Jan Olucak <ac133867@uni-stuttgart.de>
2025-11-18 19:39:25 +0100

    added first version on basics about CaSoS

---
+++ Getting Started/Basics/tut01_casos_dataTypes.m
@@ -0,0 +1,66 @@
+%% -----------------------------------------------------------------------
+%
+% Short description: This tutorial describes the different CaSoS data
+%                    types, how to setup them up and how to mainpulate
+%                    them.
+%
+% 
+%   Indeterminate variables             casos.Indeterminates
+%   Symbolic polynomial variables       casos.PS.sym
+%   Nmerical polynomial variables       casos.PD
+%
+% -----------------------------------------------------------------------
+
+
+%% Indeterminate variable 
+% Are used, for example, to setup the system dynamics
+% scalar
+x = casos.Indeterminates('x');
+% vector
+y = casos.Indeterminates('y',2);
+
+% polynomial dynamics 
+f1 = x^4 + 10*x + u;
+
+f2 = y(1)^2*y(2) + y(1);
+
+%% Symbolic variables and polynomials
+
+% scalar, for example, level set
+g = casos.PS.sym('g');
+
+% a univariate polynomial up to degree 2
+p = casos.PS.sym('p',monomials(x,0:2));
+
+% a univariate polynomial with custom monomials
+p1 = casos.PS.sym('p',monomials([x x^2 x^4]));
+
+% multivariate polynomial
+p2 = casos.PS.sym('p',monomials(y,0:2));
+
+% multivariate polynomial with custom monomials
+p3 = casos.PS.sym('p',monomials([y(1)*y(2) y(1)^2 y(2)^4]));
+
+% n = 2 multivariate polynomials
+p4 = casos.PS.sym('p',monomials(y,0:2), [2 1]);
+
+% n x n with n = 2 multivariate polynomial matrix
+p5 = casos.PS.sym('p',monomials(y,0:2), [2 2]);
+
+%% getting the coefficients and sparsity pattern of a polynomial
+
+[coeffs_p2,monomials_p2] = poly2basis(p2);
+
+% from symbolic to numerical values
+p2_num = casos.PD(monomials_p2,rand(monomials_p2.nnz,1));
+
+% get the numerical coefficients only
+[coeffs_p2__num,~] = poly2basis(p2_num);
+
+%% manipulating numerical polynomials
+
+% remove small coefficients
+p2_num = casos.PD(monomials_p2,(1 - 10^-2)*randn(monomials_p2.nnz,1)+(10^-2))
+
+% remove all coefficients smaller than 1
+p2_num_reduced = remove_coeffs(p2_num,1)
---
+++ Getting Started/Basics/tut02_casos_dynamicalSystem.m
@@ -0,0 +1,58 @@
+%% -----------------------------------------------------------------------
+%
+% Short description: This tutorial describes how to use CaSoS to setup a
+%                    simple nonlinear dynamical system, compute a linear 
+%                    control law and setup a (simple) simulation.
+%
+% 
+% -----------------------------------------------------------------------
+
+
+%% Step 1) Define open-loop nonlinear system
+% indeterminate variable
+x = casos.Indeterminates('x');
+u = casos.Indeterminates('u');
+
+% polynomial dynamics
+f = x^4 + 10*x + u;
+
+%% Step 2) Compute an LQR controller
+
+% Step 2.1: linearize system using CaSoS
+A = nabla(f,x);
+B = nabla(f,u);
+
+A0 = full(subs(A,[x;u],zeros(2,1)));
+B0 = full(subs(B,[x;u],zeros(2,1)));
+
+% Step 2.2 let us design a simple LQR controller
+[K,P] = lqr(A0,B0,1,1);
+
+
+% Step 2.3 generate the closed-loop dynamics
+f_cl = subs(f,u,-K*x);
+
+% Step 2.4 make a casos function of the closed-loop system for repeated evaluation
+f_cl_fun = to_function(f_cl);
+
+%% Step 3) simulation (simple)
+dt    = 0.1; % step size
+T     = 10; % simulation duration
+steps = linspace(0,T,T/dt);
+% allocate
+x     = zeros(1,length(steps));
+
+% inital state
+x(1) = 1;
+
+for k = 2:length(steps)
+ % Euler-forward
+ x(k) = x(k-1) + dt*full(f_cl_fun(x(k-1)));
+
+end
+
+% plotting
+figure('Name','Closed-loop Trajectory')
+plot(steps,x)
+
+
